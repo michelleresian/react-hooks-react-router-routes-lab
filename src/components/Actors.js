6df45badd70c3e2de1724299fc6a8a1f888449b3
Michelle Resian <you@example.com>
2024-02-18 12:08:14 +0300

    reactrouter

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { actors } from "../data";
+import { actors } from "../data"; // Remove the import of movies from this line
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>Name: {actor.name}</h2>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((movie, movieIndex) => (
+              <li key={movieIndex}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
